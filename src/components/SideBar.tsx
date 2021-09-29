b2c361dee3a9f2ca8be503471bc0d34872bbf0f0
Drielly Hejdenberg <driellys@live.com>
2021-09-29 15:44:44 +0200

    components + context

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
+import  { useContext } from 'react';
+import { Button } from './Button';
+import { MovieContext } from '../context/ApiContext';
+
+
+import './styles/sidebar.scss';
+
+
 export function SideBar() {
-  // Complete aqui
+  const {genres, handleClickButton, selectedGenreId} = useContext(MovieContext)
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+      </nav>
+  )
 }
